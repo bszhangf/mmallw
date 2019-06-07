0c450029f11af76038ad104f6f0b3751cb8c3c7e
zhangbs <bszhangf>
2019-06-07 20:48:15 +0800

    0607-commit-002

---
+++ src/main/java/com/test/one/TestOne.java
@@ -0,0 +1,10 @@
+package com.test.one;
+
+public class TestOne {
+
+    public static void main(String args[]){
+
+        System.out.println("Hello World !!!");
+
+    }
+}
---
+++ src/main/java/com/test/two/TestTwo.java
@@ -0,0 +1,11 @@
+package com.test.two;
+
+public class TestTwo {
+
+    public static void main(String args[]){
+
+        System.out.println("你好呀！！！");
+
+    }
+
+}
